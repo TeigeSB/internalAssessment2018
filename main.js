e4798ecb00d354c6768f5d8a756a088b4bc2f4e9
TeigeSB <teigeb0330@gmail.com>
2018-01-30 10:15:27 -0800

    testing

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-var 
+const acctSid = 'ACa06b90b0b052386d0493842a41023491';
+const authToken = 'a70ee2f50a025618ca2b7abd11622402';
+const client = require('twilio')(acctSid, authToken);
+
+client.messages
+  .create({
+    body: "something",
+    to: "+15106935050"
+    from: "+15107579255"
+  })
+  .then(message => process.stdout.write(message.sid));
