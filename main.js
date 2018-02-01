59168e66c307acb197cd31310370217dd8a95a34
TeigeSB <teigeb0330@gmail.com>
2018-02-01 09:53:09 -0800

    json stuff

--- main.js
+++ main.js
@@ -2,10 +2,12 @@ const acctSid = 'ACa06b90b0b052386d0493842a41023491';
 const authToken = 'a70ee2f50a025618ca2b7abd11622402';
 const client = require('twilio')(acctSid, authToken);
 
+var whatSend;
+
 client.messages
   .create({
-    body: "something",
-    to: "+15106935050"
+    body: whatSend,
+    to: "+15106935050",
     from: "+18316099815"
   })
   .then(message => process.stdout.write(message.sid));
