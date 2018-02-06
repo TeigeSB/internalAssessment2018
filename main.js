ad0dd04f0f5c9bace98706e687bb9eac1878d5fb
TeigeSB <teigeb0330@gmail.com>
2018-02-06 10:31:48 -0800

    fix path source routing shit

--- main.js
+++ main.js
@@ -7,12 +7,22 @@ var mediaSend;
 var receiverNumber = document.getElementById("receiverNumber").value;
 
 function sendMessage() {
-client.messages
-  .create({
-    body: '+1'whatSend,
-    to: receiverNumber,
-    from: "+18316099815"
-    mediaUrl: mediaSend
-  })
+  
+  client.messages
+    .create({
+      body: '+1'whatSend,
+      to: receiverNumber,
+      from: "+18316099815"
+      mediaUrl: mediaSend
+    })
   .then(message => process.stdout.write(message.sid));
+}
+
+function checkEmpty {
+  if (document.getElementById("receiverNumber1").value == "") {
+    prompt("Please put a number you want to send the message to!")
+  } else if (document.getElementBy("whatSend").value == "") {
+    prompt("Please put a message to send!")
+  }
+
 }
